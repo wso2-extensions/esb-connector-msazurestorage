15c4e85998b94899e325603daab396f5f1c36767
Leandro Toledo <leandro.richard.go@gmail.com>
2020-08-28 18:28:25 -0300

    Implemented queue add message operation;

--- src/main/java/org/wso2/carbon/connector/azure/storage/QueuesAddMessage.java
+++ src/main/java/org/wso2/carbon/connector/azure/storage/QueuesAddMessage.java
@@ -20,22 +20,15 @@ package org.wso2.carbon.connector.azure.storage;
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.queue.CloudQueue;
 import com.microsoft.azure.storage.queue.CloudQueueClient;
-import org.apache.axiom.om.OMAbstractFactory;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMFactory;
-import org.apache.axiom.om.OMNamespace;
+import com.microsoft.azure.storage.queue.CloudQueueMessage;
 import org.apache.synapse.MessageContext;
 import org.wso2.carbon.connector.azure.storage.util.AzureConstants;
-import org.wso2.carbon.connector.azure.storage.util.ResultPayloadCreator;
 import org.wso2.carbon.connector.core.AbstractConnector;
-
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
 
 /**
  * This class for performing list containers operation.
  */
-public class QueuesRetriever extends AbstractConnector {
+public class QueuesAddMessage extends AbstractConnector {
 
     public void connect(MessageContext messageContext) {
         if (messageContext.getProperty(AzureConstants.PROTOCOL) == null ||
@@ -47,32 +40,32 @@ public class QueuesRetriever extends AbstractConnector {
         String protocol = messageContext.getProperty(AzureConstants.PROTOCOL).toString();
         String accountName = messageContext.getProperty(AzureConstants.ACCOUNT_NAME).toString();
         String accountKey = messageContext.getProperty(AzureConstants.ACCOUNT_KEY).toString();
+        String queueName = messageContext.getProperty(AzureConstants.QUEUE_NAME).toString();
 
         String storageConnectionString = AzureConstants.PROTOCOL_KEY_PARAM + protocol + AzureConstants.SEMICOLON +
                 AzureConstants.ACCOUNT_NAME_PARAM + accountName + AzureConstants.SEMICOLON
                 + AzureConstants.ACCOUNT_KEY_PARAM + accountKey + AzureConstants.SEMICOLON;
-
-        OMFactory factory = OMAbstractFactory.getOMFactory();
-        OMNamespace ns = factory.createOMNamespace(AzureConstants.AZURE_NAMESPACE, AzureConstants.NAMESPACE);
-        OMElement result = factory.createOMElement(AzureConstants.RESULT, ns);
-        ResultPayloadCreator.preparePayload(messageContext, result);
-        String outputResult;
         try {
-            CloudStorageAccount account = CloudStorageAccount.parse(storageConnectionString);
-            CloudQueueClient queueClient = account.createCloudQueueClient();
-            for (CloudQueue queue : queueClient.listQueues()) {
-                outputResult = queue.getName();
-                OMElement messageElement = factory.createOMElement(AzureConstants.QUEUE, ns);
-                messageElement.setText(outputResult);
-                result.addChild(messageElement);
-            }
-        } catch (URISyntaxException e) {
-            handleException("Invalid input URL found.", e, messageContext);
-        } catch (InvalidKeyException e) {
-            handleException("Invalid account key found.", e, messageContext);
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount =
+                    CloudStorageAccount.parse(storageConnectionString);
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.getQueueReference(queueName);
+
+            // Create the queue if it doesn't already exist.
+            queue.createIfNotExists();
+
+            String messageToAdd = messageContext.getEnvelope().getBody().toString();
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(messageToAdd);
+            queue.addMessage(message);
+
         } catch (Exception e) {
             handleException("Exception: ", e, messageContext);
         }
-        messageContext.getEnvelope().getBody().addChild(result);
     }
 }
